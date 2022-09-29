7862a9ba4b06e7766e1ca0e6fbbc43c3fcfe14cd
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-09-29 15:20:38 -0500

    configure redux store

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+import { Provider } from 'react-redux'
+import { configureStore, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+import allReducers from './reducers/manageDaycareAndToddlerReducers';
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = configureStore(allReducers, composeEnhancer(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
