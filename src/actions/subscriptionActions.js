b53940f09895e01cd1587511eba094de0e78b36f
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-10-05 13:07:35 -0500

    build subscription action

--- src/actions/subscriptionActions.js
+++ src/actions/subscriptionActions.js
@@ -1,10 +1,10 @@
-export const fetchDaycares = () => {
+export const fetchSubscriptions = () => {
     return (dispatch) => {
-      dispatch({ type: "LOADING_DAYCARES" });
-      fetch("http://localhost:3000/daycares")
+      dispatch({ type: "LOADING_SUBSCRIPTIONS" });
+      fetch("http://localhost:3000/subscriptions")
         .then(response => response.json())
         .then((data) => {
-          dispatch({ type: "ADD_DAYCARES", daycares: data });
+          dispatch({ type: "ADD_SUBSCRIPTIONS", subscriptions: data });
         });
     };
-  };
+  };  
