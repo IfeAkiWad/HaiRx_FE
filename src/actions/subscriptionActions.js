a4655c01c88b12eb4a8cc0a3c6696118eda10575
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-10-05 13:17:18 -0500

    build subscription action

--- src/actions/subscriptionActions.js
+++ src/actions/subscriptionActions.js
@@ -7,4 +7,21 @@ export const fetchSubscriptions = () => {
           dispatch({ type: "ADD_SUBSCRIPTIONS", subscriptions: data });
         });
     };
-  };  
+};  
+
+export const deleteSubscriptions = (subscriptionId) => {
+  return async (dispatch) => {
+    const response = await fetch(`http://localhost:3000/subscriptions/${subscriptionId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify()
+    });
+    const subscription = await response.json();
+    console.log(subscription, "deleting subscription");
+    dispatch({ type: 'REMOVE_SUBSCRIPTION', payload: subscription });
+    alert(subscription.message);
+  }
+}
