4dd37082d06d65262970875a73b49fc59cddb7db
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-10-05 12:52:51 -0500

    build subscription reducer

--- src/reducers/subscriptionReducer.js
+++ src/reducers/subscriptionReducer.js
@@ -1,5 +1,26 @@
-const subscriptionReducer = () => {
-
+const subscriptionReducer = (state = {subscriptions: []}, action) => {
+    switch (action.type) {
+        case 'LOADING_SUBSCRIPTIONS':
+            return {
+                ...state,
+                subscriptions: [...state.subscriptions, action.payload],
+                loading: true
+            }   
+        case 'ADD_SUBSCRIPTIONS':
+            return {
+                ...state,
+                subscriptions: action.payload,
+                loading: false
+            }  
+        case 'REMOVE_SUBSCRIPTIONS':
+            let deleteSubscriptions = state.subscriptions.filter(sub => sub.id === action.payload.id)
+            return {
+                ...state,
+                subscriptions: deleteSubscriptions
+            }  
+        default:
+          return state;  
+    }
 }
 
 export default subscriptionReducer
