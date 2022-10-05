4b0496e0041946cd253774c84936140238cb35d4
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-10-05 12:31:04 -0500

    import reducers

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import { configureStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
 
-import allReducers from './reducers/manageDaycareAndToddlerReducers';
+import allReducers from './reducers/manageReducers';
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = configureStore(allReducers, composeEnhancer(applyMiddleware(thunk)));
