cd2888d28c7564ab4e0954201d3307bf1d09d0eb
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2023-01-20 11:54:39 -0600

    build user landing

--- src/components/usercomp/UserLanding.js
+++ src/components/usercomp/UserLanding.js
@@ -6,6 +6,9 @@ const UserLanding = () => {
         <div id='img'>
           <img  className='background-img' src={background} alt="img..." />
        </div>
+       <h1 id='user-landing-text'>
+        kkgkghgkgkgkgkgkg
+       </h1>
     </div>
   )
 }
