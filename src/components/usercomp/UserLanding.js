5b4336cdea00c36f53333c75945fe5470c4b2882
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2023-01-21 14:34:32 -0600

    build user landing

--- src/components/usercomp/UserLanding.js
+++ src/components/usercomp/UserLanding.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import background from '../../hairx model images/background.jpg'
 const UserLanding = () => {
+
   return (
     <div>
         <div id='img'>
           <img  className='background-img' src={background} alt="img..." />
        </div>
        <h1 id='user-landing-text'>
-        kkgkghgkgkgkgkgkg
+        Healthy Hair starts <a id="product-link" href="http://localhost:3001/products">HERE</a>
        </h1>
+       
     </div>
   )
 }
