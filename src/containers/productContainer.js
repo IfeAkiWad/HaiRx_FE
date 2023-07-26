25bb7917249e787487faf14e2816bc0a8e8d5300
Ifeoluwa Akinremi-Wade <ifeoluwa.wade@gmail.com>
2023-07-26 15:08:43 -0500

    house keeping after a mild hiatus

--- src/containers/productContainer.js
+++ src/containers/productContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import ProductFilterContainer from '../containers/ProductFilterContainer'
+import ProductFilterContainer from './ProductFilterContainer'
 
 
 class ProductContainer extends Component {
